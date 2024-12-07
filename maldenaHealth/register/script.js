5077154d0981948a7fd6cc17ed39491523f13b7c
Nhm1Off <nazar75xz@gmail.com>
2024-12-07 18:44:36 +0200

    Add files via upload

---
+++ maldenaHealth/register/script.js
@@ -0,0 +1,30 @@
+window.onload = function() {
+    google.accounts.id.initialize({
+        client_id: "672903744712-5s97ekr693ere7g2vk7c89jc9cacvugt.apps.googleusercontent.com", // Вставте ваш Client ID
+        callback: handleCredentialResponse
+    });
+
+    google.accounts.id.prompt();
+};
+
+function handleCredentialResponse(response) {
+    console.log("Encoded JWT ID token: " + response.credential);
+
+    // Надсилаємо токен на сервер для перевірки
+    fetch('https://www.unis2.store/maldenaHealth/profile', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ token: response.credential })
+    })
+    .then(res => res.json())
+    .then(data => {
+        console.log('User successfully authenticated:', data);
+        alert('You are logged in');
+    })
+    .catch(error => {
+        console.error('Error:', error);
+        alert('Error during sign-in');
+    });
+}
