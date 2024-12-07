028d6d6c3799f6aa8da44e4683f7c6cdcc347089
Nhm1Off <nazar75xz@gmail.com>
2024-12-07 21:16:34 +0200

    Update script.js

--- maldenaHealth/register/script.js
+++ maldenaHealth/register/script.js
@@ -1,30 +1,54 @@
-window.onload = function() {
-    google.accounts.id.initialize({
-        client_id: "672903744712-1dhajcnv9bm36bbbc746pdi3cs0l2r5m.apps.googleusercontent.com", // Вставте ваш Client ID
-        callback: handleCredentialResponse
-    });
+const nameInput = document.querySelector("#name");
+const regEmail = document.querySelector("#email");
+const Registerpassword = document.querySelector("#password");
+const regBtn = document.querySelector("#registerBtn");
 
-    google.accounts.id.prompt();
+
+
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.2/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/11.0.2/firebase-analytics.js";
+import { getAuth, createUserWithEmailAndPassword, updateProfile } from "https://www.gstatic.com/firebasejs/11.0.2/firebase-auth.js"; // Імпортуємо методи аутентифікації
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCkbg5rtAZ9SV321Djga4xBwsTG2uOUpjw",
+  authDomain: "maldenahealth-finally.firebaseapp.com",
+  projectId: "maldenahealth-finally",
+  storageBucket: "maldenahealth-finally.firebasestorage.app",
+  messagingSenderId: "494931005513",
+  appId: "1:494931005513:web:72dd11f631433967c9ef98",
+  measurementId: "G-YH5DSD3L77"
 };
 
-function handleCredentialResponse(response) {
-    console.log("Encoded JWT ID token: " + response.credential);
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
-    // Надсилаємо токен на сервер для перевірки
-    fetch('https://www.unis2.store/maldenaHealth', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({ token: response.credential })
-    })
-    .then(res => res.json())
-    .then(data => {
-        console.log('User successfully authenticated:', data);
-        alert('You are logged in');
-    })
-    .catch(error => {
-        console.error('Error:', error);
-        alert('Error during sign-in');
-    });
+const auth = getAuth(app);
+
+// Код
+const userReg = async (event) => {
+    event.preventDefault();
+
+    try {
+        const email = regEmail.value;
+        const password = Registerpassword.value;
+
+        await createUserWithEmailAndPassword(auth,email,password).catch((error) => {
+            alert(error)
+        })
+
+        await updateProfile(auth.currentUser, {
+            displayName: nameInput.value
+        })
+        alert("Account has been created")
+        window.location.replace("http://127.0.0.1:5500/")
+    } catch (error) {
+        alert(error) 
+    }
 }
+
+regBtn.addEventListener("click",  userReg)
