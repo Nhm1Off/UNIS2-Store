22647b5ad1ba8f680b86e282f8376d9181290d41
Nhm1Off <nazar75xz@gmail.com>
2024-12-07 19:02:29 +0200

    Update script.js

--- maldenaHealth/register/script.js
+++ maldenaHealth/register/script.js
@@ -11,7 +11,7 @@ function handleCredentialResponse(response) {
     console.log("Encoded JWT ID token: " + response.credential);
 
     // Надсилаємо токен на сервер для перевірки
-    fetch('https://www.unis2.store/maldenaHealth/profile', {
+    fetch('https://www.unis2.store/maldenaHealth/profile/index.html', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
