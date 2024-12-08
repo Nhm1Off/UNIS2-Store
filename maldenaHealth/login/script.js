4cc596ab33d619d5260eea984b1c231b9a5e2b56
Nhm1Off <nazar75xz@gmail.com>
2024-12-08 12:38:34 +0200

    Update script.js

--- maldenaHealth/login/script.js
+++ maldenaHealth/login/script.js
@@ -5,20 +5,21 @@ const signinPasswordInput = document.querySelector("#SignInPassword");
 
 import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.2/firebase-app.js";
 import { getAnalytics } from "https://www.gstatic.com/firebasejs/11.0.2/firebase-analytics.js";
-import { getAuth, createUserWithEmailAndPassword, updateProfile, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/11.0.2/firebase-auth.js"; // Імпортуємо методи аутентифікації
+import { getFirestore, setDoc, doc } from "https://www.gstatic.com/firebasejs/11.0.2/firebase-firestore.js";
+import { getAuth, createUserWithEmailAndPassword, updateProfile, signInWithEmailAndPassword, signOut, GoogleAuthProvider, signInWithPopup } from "https://www.gstatic.com/firebasejs/11.0.2/firebase-auth.js"; // Імпортуємо методи аутентифікації
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCkbg5rtAZ9SV321Djga4xBwsTG2uOUpjw",
-  authDomain: "maldenahealth-finally.firebaseapp.com",
-  projectId: "maldenahealth-finally",
-  storageBucket: "maldenahealth-finally.firebasestorage.app",
-  messagingSenderId: "494931005513",
-  appId: "1:494931005513:web:72dd11f631433967c9ef98",
-  measurementId: "G-YH5DSD3L77"
+    apiKey: "AIzaSyCkbg5rtAZ9SV321Djga4xBwsTG2uOUpjw",
+    authDomain: "maldenahealth-finally.firebaseapp.com",
+    projectId: "maldenahealth-finally",
+    storageBucket: "maldenahealth-finally.firebasestorage.app",
+    messagingSenderId: "494931005513",
+    appId: "1:494931005513:web:72dd11f631433967c9ef98",
+    measurementId: "G-YH5DSD3L77"
 };
 
 // Initialize Firebase
@@ -37,7 +38,7 @@ const userSignin = (e) => {
             alert("Signed in succesfully!")
             window.location.replace("https://unis2.store/maldenaHealth/profile")
 
-        }).catch( (error) => {
+        }).catch((error) => {
             alert(error);
         })
     } catch (error) {
@@ -45,4 +46,65 @@ const userSignin = (e) => {
     }
 }
 
+const logoutUser = () => {
+    try {
+        signOut(auth).then(() => {
+            alert("Signed out!");
+            window.location.replace("https://unis2.store/maldenaHealth/login");
+        })
+    } catch (error) {
+        alert(error);
+    }
+}
+
+
 signinBtn.addEventListener("click", userSignin);
+
+const provider = new GoogleAuthProvider();
+
+const googleLogin = document.getElementById("google-login-btn");
+googleLogin.addEventListener("click", function () {
+    signInWithPopup(auth, provider)
+        .then((result) => {
+            const credential = GoogleAuthProvider.credentialFromResult(result);
+            const user = result.user;
+            console.log(user);
+            window.location.href = "https://unis2.store/maldenaHealth/profile";
+
+        }).catch((error) => {
+            const errorCode = error.code;
+            const errorMessage = error.message;
+        });
+
+})
+
+function updateUserProfile(user) {
+    if (user) {
+        const userName = user.displayName || "No name available";
+        const userEmail = user.email || "No email available";
+        const userProfilePicture = user.photoURL || "default-profile-picture-url.jpg";
+
+        // Переконатись, що елементи DOM існують перед їх використанням
+        const userNameElement = document.getElementById("userName");
+        const userEmailElement = document.getElementById("userEmail");
+        const userProfilePictureElement = document.getElementById("userProfilePicture");
+
+        if (userNameElement) userNameElement.textContent = userName;
+        if (userEmailElement) userEmailElement.textContent = userEmail;
+        if (userProfilePictureElement) userProfilePictureElement.src = userProfilePicture;
+    } else {
+        console.error("User object is undefined or null.");
+    }
+}
+
+updateUserProfile()
+
+onAuthStateChanged(auth, (user) => {
+    if (user) {
+        // Якщо користувач авторизований, оновіть профіль
+        updateUserProfile(user);
+    } else {
+        // Якщо користувач не авторизований
+        alert("Create account & Login");
+    }
+});
