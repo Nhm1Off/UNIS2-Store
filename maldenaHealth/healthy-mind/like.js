592cb8a71d9199335579c6d8cdef082dab172b1c
Nhm1Off <nazar75xz@gmail.com>
2024-12-18 10:45:05 +0200

    Update like.js

--- maldenaHealth/healthy-mind/like.js
+++ maldenaHealth/healthy-mind/like.js
@@ -1,30 +1,25 @@
 const likeBtn = document.getElementById('likeBtn');
 const likeImg = document.getElementById('likeImg');
 
-let liked = false;  // Спочатку лайк не поставлений
+let liked = false
 
 function AnimLikeImg() {
-    // Скидаємо поточну анімацію
     likeImg.style.animation = 'none';
 
-    // Невелика затримка перед початком анімації
     setTimeout(() => {
         likeImg.style.animation = 'like-animation 0.6s ease';
 
-        // Після завершення анімації змінюємо картинку
         likeImg.addEventListener('animationend', () => {
             if (liked) {
-                // Якщо лайк вже поставлений, скидаємо картинку на "не лайк"
-                likeImg.src = 'https://i.ibb.co/4mLP4WC/image.png';  // Початкова картинка
-                liked = false;  // Скидаємо стан лайка
+                likeImg.src = 'https://i.ibb.co/tMtHPp3/image.png';  
+                liked = false; 
             } else {
-                // Якщо лайк не поставлений, ставимо картинку лайка
-                likeImg.src = 'https://i.ibb.co/wpqbNmx/image.png';  // Лайкнута картинка
-                liked = true;  // Встановлюємо стан лайка
+
+                likeImg.src = 'https://i.ibb.co/wpqbNmx/image.png'; 
+                liked = true; 
             }
-        }, { once: true }); // Запускаємо обробник лише один раз після анімації
+        }, { once: true });
     }, 10);
 }
 
-// Додаємо обробник події на натискання
 likeBtn.addEventListener("click", AnimLikeImg);
