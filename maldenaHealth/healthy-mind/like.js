3d4f8bd75f4ed851a0b88377d18d6a40c1a380d9
Nhm1Off <nazar75xz@gmail.com>
2024-12-18 22:18:05 +0200

    Update like.js

--- maldenaHealth/healthy-mind/like.js
+++ maldenaHealth/healthy-mind/like.js
@@ -9,14 +9,13 @@ function AnimLikeImg() {
     setTimeout(() => {
         likeImg.style.animation = 'like-animation 0.6s ease';
 
-     likeImg.src = 'https://i.ibb.co/wpqbNmx/image.png'; 
  
             if (liked) { 
                 likeImg.src = 'https://i.ibb.co/tMtHPp3/image.png';  
                 liked = false; 
             } else {
 
-                
+                likeImg.src = 'https://i.ibb.co/wpqbNmx/image.png'; 
                 liked = true; 
             }
         }, { once: true });
